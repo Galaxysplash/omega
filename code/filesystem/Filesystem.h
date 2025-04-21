bc1e10bef659a84b035628c7c1113ab350fb5e52
marcel <Marcel.Hajek@gmx.de>
2025-04-21 05:18:44 -0400

    ...

--- code/filesystem/Filesystem.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string_view>
-
-class Filesystem {
-public:
-        static std::string_view GetPath(const int argc, const char **argv);
-        static std::string_view ReadFile(const std::string_view &path);
-};
--- code/memory/BufferSize.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-enum EBufferSize : size_t {
-        None = 0,
-        Byte = 1,
-        KB = 1024,
-        MB = 1024 * 1024,
-        GB = 1024 * 1024 * 1024,
-};
